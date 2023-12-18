53f36bee57c92a7f79242c27e7a61a2b08dd5ae9
saadalikhan391 <123077989+saadalikhan391@users.noreply.github.com>
2023-12-18 13:22:30 -0800

    Add files via upload
    this file make understanding of print and string

---
+++ string.py
@@ -0,0 +1,67 @@
+print("this is string")   # this is string data type when ever you want to pass string data always use double/single quotes "" ''
+print('saad ali khan')    # cd stand for change directory
+print('224')
+print("this is my first line" + "this is my second line") # concatenate function use to join two stings data type string
+print("2" + "2")
+print(2 + 2)
+
+#to check the output and to print the command
+#hash use for comment it will not include in code
+#print()   parenthesis circle 
+
+#Aphabet letters "" or '' it should be open and close between that commas you have to define letter or word
+print("python first class")
+print("this is my first class")
+print('python')
+print("saad") 
+print("khan")
+#space is called indent in coding by default it take 4 indent forward slash multiline print
+print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\
+bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccc")
+
+print('Irtaza')
+
+print('111122233444')
+print('1')
+
+
+#"" or '' what ever define in single inverted comma or double this data type called String
+
+"String"
+
+print("String")
+
+print("this is string")
+print('my first string')
+
+# all data and datatype can be stored in varaiable
+# variable is for to store data
+
+s = "this is string"
+print(s)
+print(s*4)
+
+variable = "data type is String" 
+print(variable)
+
+print(s + variable)
+
+Pakistan_Capital  = "Isalamabad"
+print(Pakistan_Capital)
+
+Provience = "KPK Punjab Sindh Balochistan"
+print(Provience)
+
+Sindh = "Karachi"
+print(Sindh)
+
+country = "Pakistan"
+print(country)
+
+print(country + Sindh)
+print(country, Sindh)
+
+print("saad" + "khan")
+
+#string data type use plus sign is called concatenation or concatenate one line attach with other line 
