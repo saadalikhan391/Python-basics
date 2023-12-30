73beebe4e0cd0ec53d139d3f976a5139a8acaa8a
saadalikhan391 <123077989+saadalikhan391@users.noreply.github.com>
2023-12-30 06:40:16 -0800

    Add files via upload

---
+++ print-function.py
@@ -0,0 +1,27 @@
+#print function always give you the output of the result print()
+#comment
+#single inverted comma double inverrted comma both performing same thing there is no such difference in python
+#String data "" or '' only in python it perform same 
+#"any alphabet any number"
+# cd stand for change directory  backward cd .. cd path name  ls mean list directory
+#tab key help you to choose the path 
+#run file python and name of file
+print('python code')
+print("this is my first class")
+#shortcut key in windows is F5
+print('aaaaaa')
+print("saad","Trainer","python")   #for seperation use comma ,
+
+print("ssssssssssssssssssssssssssssssssssssssssssssssssssss\
+sssssssssssssssss\
+ssssssssssssss")    
+
+print("today is my \n second class\n of the python\n and i am learing\n about print function")
+
+print("karachi","lahore","Islamabad")
+
+print('Karachi \nLahore \nIslamabad' )
+
+name = input("whats your name:")
+print("hello",name)
+print(name)
