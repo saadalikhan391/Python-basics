7ec49a1c217fba6258d30558e352c5e59e8ed9de
saadalikhan391 <123077989+saadalikhan391@users.noreply.github.com>
2023-12-31 07:48:45 -0800

    Add files via upload

--- print-function.py
+++ print-function.py
@@ -22,6 +22,77 @@ print("karachi","lahore","Islamabad")
 
 print('Karachi \nLahore \nIslamabad' )
 
-name = input("whats your name:")
-print("hello",name)
-print(name)
+#input function always represent with input()
+#vaiable is a box where you store data inside it
+
+# name = input("whats your name:")
+# print("hello",name)
+# print(name)
+
+box = "python"
+box1 = "html"
+box2 = "java"
+box3 = "javascript"
+box4 = "php"
+box5 = "python"
+
+print(box,box1,box2,box3,box4,box5)
+print("programing language is:",box)
+print(box1 + box)   #concatenate it mean join one line to another
+#multiline variables
+a,b,c = 'A','B','C'
+
+print(a,b,c)
+
+#always initialized variable with alphabet or underscore 
+_s = "saad"
+
+print(_s)
+
+# = sign asign the value  == sign is comparision operator 
+food = input("do you like fruit?")
+
+if food == "yes":
+    print("great! good for health")
+elif food == "no":
+    print("that sad i've something for you")
+else:
+    print("select yes/no")
+
+
+#Python String literal
+
+# in single quote
+s = 'python by khan'
+ 
+# in double quotes
+t = "python by khan"
+ 
+# multi-line String
+m = '''python 
+           by 
+               khan'''
+ 
+print(s)
+print(t)
+print(m)
+
+
+#format method print
+
+print('I love {} by "{}!"'.format('python', 'khan'))
+print('programming language {}'.format('Java'))
+ 
+# # using format() method and referring a position of the object
+print('{0} and {1}'.format('python', 'Java'))
+ 
+print('{1} and {0}'.format('python', 'Java'))
+ 
+print(f"I love {'python'} for \"{'Java'}!\"")
+ 
+# # using format() method and referring a position of the object
+print(f"{'python'} and {'java'}")
+
+
+
+
